914729ed19bb88c07387e9da9ad940a90e45c0f4
LeoRoA <romanleontev@gmail.com>
2022-09-22 19:59:13 +0700

    На проверку ДЗ №3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("Задание 1");
+        int vklad1 = 29000;
+        int sum1 = 0;
+        for (int i = 1; i <= 12; i++ ){
+            sum1 = sum1 + vklad1;
+            System.out.printf ("Месяц %d, сумма накоплений равна %d рублей\n", i, sum1);
+        }
+        //Задание 2
+        System.out.println("Задание 2");
+        int vklad2 = 29000;
+        int sum2 = 0;
+        for (int i = 1; i <= 12; i++ ){
+            sum2=sum2 +sum2/100 +vklad2;
+            System.out.printf ("Месяц %d, сумма накоплений равна %d рублей\n", i, sum2);
+        }
     }
 }
